55b957dce574061dda11348fe54dc0b43ec1dfc1
Ruizhang Zhou <ruizhangzhourick@gmail.com>
2021-05-03 17:29:06 +0200

    fix stack inconsistency

--- versuch2/SPOS/SPOS/os_scheduler.c
+++ versuch2/SPOS/SPOS/os_scheduler.c
@@ -81,7 +81,7 @@ ISR(TIMER2_COMPA_vect) {
 	//nach dem Setzen des Stackpointers auf den Scheduler-Stack die Prüfsumme des Prozessstacks des unterbrochenen Prozesses ermittelt und abspeichert.
     os_processes[os_getCurrentProc()].checksum = os_getStackChecksum(os_getCurrentProc());
     
-	os_initInput(); 
+	
     if(os_getInput() == 0b00001001) {
 		os_waitForNoInput();
 		os_taskManMain();
